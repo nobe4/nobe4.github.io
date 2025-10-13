b822aa25c0b372169987373224c1ee79ffa1f91d
nobe4 <nobe4@users.noreply.github.com>
2025-10-13 21:21:52 +0200

    feat: improve memory pairs game

--- content/pages/memory/pairs/js/main.js
+++ content/pages/memory/pairs/js/main.js
@@ -1,11 +1,6 @@
 const board = document.getElementById("board");
 
-let available_colors = [
-	["aqua", "A"],
-	["lime", "L"],
-	["yellow", "Y"],
-	["fuchsia", "F"],
-];
+let available_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 let active_cards = [];
 let solved_pairs = 0;
 
@@ -13,6 +8,7 @@ let start;
 
 window.gen = () => {
 	board.innerHTML = "";
+	total.innerText = `Total: ${value_count.value * letter_count.value * 2}`;
 
 	gen_items(value_count.value).forEach(gen_card);
 };
@@ -31,7 +27,6 @@ window.click_card = (e) => {
 	}
 
 	e.target.classList.toggle("active");
-	e.target.style.backgroundColor = e.target.getAttribute("data-c");
 	e.target.innerHTML = e.target.getAttribute("data-v");
 	active_cards.push(e.target);
 
@@ -39,7 +34,7 @@ window.click_card = (e) => {
 		check_active_cards();
 	}
 
-	if (solved_pairs == value_count.value * color_count.value) {
+	if (solved_pairs == value_count.value * letter_count.value) {
 		let ms = Date.now() - start;
 		let minutes = Math.floor(ms / 60000);
 		let seconds = ((ms % 60000) / 1000).toFixed(0);
@@ -50,13 +45,6 @@ window.click_card = (e) => {
 	}
 };
 
-function cards_equal(a, b) {
-	return (
-		a.getAttribute("data-v") == b.getAttribute("data-v") &&
-		a.getAttribute("data-c") == b.getAttribute("data-c")
-	);
-}
-
 function clean_active_cards() {
 	for (let e of active_cards) {
 		e.innerHTML = "";
@@ -66,18 +54,18 @@ function clean_active_cards() {
 }
 
 function check_active_cards() {
-	if (!cards_equal(active_cards[0], active_cards[1])) {
+	if (
+		active_cards[0].getAttribute("data-v") !=
+		active_cards[1].getAttribute("data-v")
+	) {
 		return;
 	}
 
 	for (let e of active_cards) {
-		if (delete_on_match.checked) {
-			debugger;
-		} else {
-			e.removeEventListener("click", click_card);
-			e.innerHTML = "✓";
-			e.style.backgroundColor = "white";
-		}
+		e.removeEventListener("click", click_card);
+		e.innerHTML = "✓";
+		e.classList.toggle("active");
+		e.classList.toggle("solved");
 	}
 
 	solved_pairs++;
@@ -86,12 +74,12 @@ function check_active_cards() {
 
 function gen_items(count) {
 	let items = [];
-	const colors = available_colors.slice(0, color_count.value);
+	const colors = available_letters.slice(0, letter_count.value);
 
 	for (let i = 0; i < count; i++) {
 		for (let c of colors) {
-			items.push({ v: i, c: c });
-			items.push({ v: i, c: c });
+			items.push(`${i}${c}`);
+			items.push(`${i}${c}`);
 		}
 	}
 
@@ -114,11 +102,10 @@ function shuffle(array) {
 	return array;
 }
 
-function gen_card({ v, c }) {
+function gen_card(v) {
 	let card = document.createElement("div");
 	card.classList.add("card");
-	card.setAttribute("data-v", v + c[1]);
-	card.setAttribute("data-c", c[0]);
+	card.setAttribute("data-v", `${v}`);
 	card.addEventListener("click", click_card);
 	board.appendChild(card);
 }
