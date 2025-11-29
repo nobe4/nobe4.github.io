b055023611cc0edfcfc0ea6cabc21c57932271a3
nobe4 <nobe4@users.noreply.github.com>
2025-11-29 17:46:37 +0100

    feat: more flags

--- content/posts/great-flag-unification/index.md
+++ content/posts/great-flag-unification/index.md
@@ -2,5 +2,8 @@
 title = "Great Flag Unification"
 layout = "great_flag_unification"
 date = "2025-11-26"
-draft = true
+references = [
+    "https://flagpedia.net/",
+]
 +++
+
--- content/posts/great-flag-unification/js/main.js
+++ content/posts/great-flag-unification/js/main.js
@@ -26,7 +26,8 @@ const fn = {
 		x *= INC;
 		y *= INC;
 		r *= INC;
-		a *= Math.PI / 180;
+		a -= 2;
+		a *= (45 * Math.PI) / 180;
 
 		const a0 = a;
 		const a1 = a + (2 * Math.PI) / 3;
@@ -47,27 +48,86 @@ const fn = {
 	},
 };
 
-const draw = (canvas, code) => {
-	let ctx = canvas.getContext("2d");
-	canvas.width = SIZE;
-	canvas.height = SIZE;
+const fn2 = {
+	r: (x, y, w, h) => {
+		let e = document.createElementNS("http://www.w3.org/2000/svg", "rect");
 
-	fn["r"](ctx, 0, 0, DIVISIONS, DIVISIONS, "*", "white");
+		e.setAttribute("x", x);
+		e.setAttribute("y", y);
+		e.setAttribute("width", w);
+		e.setAttribute("height", h);
+
+		return e;
+	},
+	c: (x, y, w, h) => {
+		let e = document.createElementNS("http://www.w3.org/2000/svg", "ellipse");
+		let rx = w;
+		let ry = h;
+
+		e.setAttribute("cx", x + rx);
+		e.setAttribute("cy", y + ry);
+		e.setAttribute("rx", rx);
+		e.setAttribute("ry", ry);
+
+		return e;
+	},
+	t: (x, y, w, h) => {
+		let e = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+		let points = [
+			[x, y],
+			[x + w, y],
+			[x + w / 2, y + h],
+		]
+			.map((p) => p.join(","))
+			.join(" ");
+		e.setAttribute("points", points);
+		return e;
+	},
+};
+
+const draw2 = (svg, code) => {
+	svg.setAttribute("width", "100");
+	svg.setAttribute("height", "100");
 
 	code.split("\n").forEach((line) => {
-		let [f, x, y, w, h, t, color] = line.replace(normalizeRe, " ").split(" ");
+		let [f, x, y, w, h, a, t, c] = line.replace(normalizeRe, " ").split(" ");
 
 		if (w == "*") w = DIVISIONS;
 		if (h == "*") h = DIVISIONS;
+		a = parseFloat(a);
 
-		ctx.strokeStyle = color;
-		ctx.fillStyle = color;
-		ctx.lineWidth = 1;
+		x = parseFloat(x) * INC;
+		y = parseFloat(y) * INC;
+		w = parseFloat(w) * INC;
+		h = parseFloat(h) * INC;
+		const center = [x + w / 2, y + h / 2];
 
-		fn[f](ctx, parseFloat(x), parseFloat(y), parseFloat(w), parseFloat(h), t);
+		let e = fn2[f](x, y, w, h);
+
+		if (t == "*") {
+			e.setAttribute("fill", c);
+		} else {
+			e.setAttribute("fill", "transparent");
+			e.setAttribute("stroke", c);
+			e.setAttribute("stroke-width", t);
+		}
+
+		if (a > 0) {
+			e.setAttribute(
+				"transform",
+				`rotate(${a * 45} ${center[0]} ${center[1]})`,
+			);
+		}
+
+		svg.appendChild(e);
 	});
 };
 
+document.querySelectorAll(".flag").forEach((e) => {
+	e.addEventListener("click", () => e.classList.toggle("simple"));
+});
+
 document.querySelectorAll('code[data-lang="flag"]').forEach((e) => {
-	draw(e.parentNode.parentNode.querySelector("canvas"), e.innerText.trim());
+	console.log(e);
+	draw2(e.parentNode.parentNode.querySelector("svg"), e.innerText.trim(""));
 });
