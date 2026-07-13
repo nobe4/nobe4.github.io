3fb3f4438aaaf963e4faa87d7f4d8dddd3949f8a
nobe4 <nobe4@users.noreply.github.com>
2026-07-13 12:05:15 +0200

    fix(hugo): couple of small fixes

--- archetypes/default.md
+++ archetypes/default.md
@@ -1,5 +1,5 @@
 +++
-title = '{{ replace .File.ContentBaseName "-" " " | title }}'
+title = 'Placeholder'
 date = {{ .Date }}
 draft = true
 +++
--- devenv.nix
+++ devenv.nix
@@ -18,10 +18,16 @@
 
   scripts.serve.exec = ''
     open http://0.0.0.0:1313
-    hugo serve --bind 0.0.0.0 --baseURL=http://0.0.0.0:1313/ --minify
+    hugo serve --bind 0.0.0.0 --baseURL=http://0.0.0.0:1313/ --minify --buildDrafts
   '';
   scripts.build.exec = "hugo build --minify";
   scripts.format.exec = "prettier -w .";
+  scripts.new.exec = ''
+    name="''${1:?provide a name}"
+    file="content/posts/$(date +%F)-''${name// /-}.md"
+    hugo new content "$file"
+    $EDITOR "$file"
+  '';
 
   git-hooks.hooks.prettier = {
     enable = true;
